75cd32158a120231d1967864839e527cfa1141a2
Robert Saternus <robert.saternus@gmail.com>
2026-02-24 14:26:42 +0100

    INT-227: Add Query Array Normalizer middleware
    Add Query Array Normalizer middleware

---
+++ src/Client/Middleware/QueryArrayNormalizer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\FactFinder\Communication\Client\Middleware;
+
+use Psr\Http\Message\RequestInterface;
+
+class QueryArrayNormalizer
+{
+    public function __invoke(callable $handler)
+    {
+        return function (RequestInterface $request, array $options) use ($handler) {
+
+            $uri   = $request->getUri();
+            $query = $uri->getQuery();
+
+            if (!$query) {
+                return $handler($request, $options);
+            }
+
+            parse_str($query, $params);
+
+            $normalized = [];
+
+            foreach ($params as $key => $value) {
+                if (is_array($value)) {
+                    foreach ($value as $v) {
+                        $normalized[] = rawurlencode($key) . '=' . rawurlencode((string) $v);
+                    }
+
+                    continue;
+                }
+
+                $normalized[] = rawurlencode($key) . '=' . rawurlencode((string) $value);
+            }
+
+            $newQuery = implode('&', $normalized);
+
+            $request = $request->withUri(
+                $uri->withQuery($newQuery)
+            );
+
+            return $handler($request, $options);
+        };
+    }
+}
